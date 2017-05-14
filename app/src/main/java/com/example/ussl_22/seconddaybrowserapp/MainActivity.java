7ba91900755ac052472da3003d1051532becf7c8
ASIF-006 <professional00001@yahoo.com>
2017-05-14 09:29:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ussl_22/seconddaybrowserapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ussl_22.seconddaybrowserapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.loadUrl("https://www.youtube.com");
+    }
+}
+
